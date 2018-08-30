7e78a84dd09e1e43c10b1c2005cbf5b170ea2f7e
NGYL <nyl162@hotmail.com>
2018-08-30 15:39:21 +0800

    varDone

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { ToDolist } from './components/to-doform.component'
-import { Moment } from 'moment';
+import { Moment, max } from 'moment';
+import { element } from '@angular/core/src/render3/instructions';
 
 @Component({
   selector: 'app-root',
@@ -10,12 +11,22 @@ import { Moment } from 'moment';
 
 export class AppComponent {
   title = 'ToDo';
-  
+  index: number = 0;
 
   MasterToDoList: ToDolist[] = [];
 
   AddListItem(newItem: ToDolist){
     console.log('App Form Event received ---', newItem);
+    console.log('MasterList Length ---', this.MasterToDoList.length);
+    
+    if(this.MasterToDoList.length<1){
+      this.index = 0;
+    }else{
+      
+      this.index = Math.max.apply(Math, this.MasterToDoList.map(function(o) { return o.taskID; }))
+    }
+    newItem.taskID = this.index + 1;
+    console.log('Final Index ', this.index + 1);
     this.MasterToDoList.push(newItem);
   }
 }
